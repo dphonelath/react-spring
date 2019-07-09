fa2fec1a570fac226bc03ffb2ad0cc6053460373
Daniel Phonelath <daniel.phonelath@gmail.com>
2019-07-09 13:54:46 -0600

    first componenet up and running

---
+++ animation-exercise-spring/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Component1 from './components/Component1';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Component1 />
+      </div>
+    )
+  }
+}
+export default App;
