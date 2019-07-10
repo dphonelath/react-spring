a4f25379ccdb1b8c3b10b806a84c4a5adf8bbfef
Daniel Phonelath <daniel.phonelath@gmail.com>
2019-07-10 12:50:17 -0600

    Made component1 and component2 with two different animations

--- animation-exercise-spring/src/components/Component1.js
+++ animation-exercise-spring/src/components/Component1.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import { Spring } from 'react-spring';
+import { useSpring, animated } from 'react-spring';
 
 export default function Component1() {
+    const props = useSpring({opacity: 1, marginTop: 0,
+                                from: {opacity: 0, marginTop: -500}})
+
     return (
-        <div style={c1Style}>
-            <h1>Component 1</h1>
-            <p>Lorem Ipsum is simply dummy text of the printing and 
-                typesetting industry. Lorem Ipsum has been the industry's 
-                standard dummy text ever since the 1500s, when an unknown 
-                printer took a galley of type and scrambled it to make 
-                a type specimen book. It has survived not only five centuries, 
-                but also the leap into electronic typesetting, remaining essentially 
-                unchanged.
-            </p>
-        </div>
+        <animated.div style={props}>
+                    <div style={c1Style}>
+                        <h1>Component 1</h1>
+                        <p>Lorem Ipsum is simply dummy text of the printing and 
+                            typesetting industry. Lorem Ipsum has been the industry's 
+                            standard dummy text ever since the 1500s, when an unknown 
+                            printer took a galley of type and scrambled it to make 
+                            a type specimen book. It has survived not only five centuries, 
+                            but also the leap into electronic typesetting, remaining essentially 
+                            unchanged.
+                        </p>
+                    </div>
+        </animated.div>
+                
     )
 }
 
---
+++ animation-exercise-spring/src/components/Component2.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { useSpring, animated } from 'react-spring'
+
+export class Component2 extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
