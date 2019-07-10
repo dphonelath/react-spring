2c1df24f835604cc5e976170ff6784a85cc3116f
Daniel Phonelath <daniel.phonelath@gmail.com>
2019-07-10 13:22:34 -0600

    Component2 working

--- animation-exercise-spring/src/components/Component1.js
+++ animation-exercise-spring/src/components/Component1.js
@@ -2,24 +2,25 @@ import React from 'react';
 import { useSpring, animated } from 'react-spring';
 
 export default function Component1() {
-    const props = useSpring({opacity: 1, marginTop: 0,
+    const props1 = useSpring({opacity: 1, marginTop: 0,
                                 from: {opacity: 0, marginTop: -500}})
+    
+    const props2 = useSpring({ number:10, from: { number: 0}})
 
     return (
-        <animated.div style={props}>
-                    <div style={c1Style}>
-                        <h1>Component 1</h1>
-                        <p>Lorem Ipsum is simply dummy text of the printing and 
-                            typesetting industry. Lorem Ipsum has been the industry's 
-                            standard dummy text ever since the 1500s, when an unknown 
-                            printer took a galley of type and scrambled it to make 
-                            a type specimen book. It has survived not only five centuries, 
-                            but also the leap into electronic typesetting, remaining essentially 
-                            unchanged.
-                        </p>
-                    </div>
+        <animated.div style={props1}>
+            <div style={c1Style}>
+                <h1>Component 1</h1>
+                <p>Lorem Ipsum is simply dummy text of the printing and 
+                    typesetting industry. Lorem Ipsum has been the industry's 
+                    standard dummy text ever since the 1500s, when an unknown 
+                    printer took a galley of type and scrambled it to make 
+                    a type specimen book. It has survived not only five centuries, 
+                    but also the leap into electronic typesetting, remaining essentially 
+                    unchanged.
+                </p>
+            </div>
         </animated.div>
-                
     )
 }
 
@@ -27,4 +28,12 @@ const c1Style = {
     background: 'steelblue',
     color: 'white',
     padding: '1.5rem'
+}
+
+const counter = {
+    backgroun: '#333',
+    textAlign: 'center',
+    width: '100px',
+    boderRadius: '50%',
+    margin: '1rem auto'
 }
--- animation-exercise-spring/src/components/Component2.js
+++ animation-exercise-spring/src/components/Component2.js
@@ -1,12 +1,30 @@
-import React, { Component } from 'react'
-import { useSpring, animated } from 'react-spring'
+import React from 'react';
+import { useSpring, animated } from 'react-spring';
 
-export class Component2 extends Component {
-    render() {
-        return (
-            <div>
-                
+export default function Component1() {
+    const props = useSpring({opacity: 1,
+                            config: { delay: 9000, duration: 1000 },
+                            from: {opacity: 0}
+                                })
+    return (
+        <animated.div style={props}>
+            <div style={c2Style}>
+                <h1>Component 1</h1>
+                <p>Lorem Ipsum is simply dummy text of the printing and 
+                    typesetting industry. Lorem Ipsum has been the industry's 
+                    standard dummy text ever since the 1500s, when an unknown 
+                    printer took a galley of type and scrambled it to make 
+                    a type specimen book. It has survived not only five centuries, 
+                    but also the leap into electronic typesetting, remaining essentially 
+                    unchanged.
+                </p>
             </div>
-        )
-    }
+        </animated.div>
+    )
+}
+
+const c2Style = {
+    background: 'slateblue',
+    color: 'white',
+    padding: '1.5rem'
 }
