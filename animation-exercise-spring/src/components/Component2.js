f360eb960f49fd49f1f0b316ba373e929d80adfe
Daniel Phonelath <daniel.phonelath@gmail.com>
2019-07-12 10:16:41 -0600

    Component 2 is still broken. Made it a class component and then imported a new functional component in Component2 called ShowComponent3 in it to pipe up to App.js. An error shows up about using useSpring

--- animation-exercise-spring/src/components/Component2.js
+++ animation-exercise-spring/src/components/Component2.js
@@ -1,43 +1,18 @@
 import React from 'react';
-import { useSpring, animated } from 'react-spring';
+import ShowComponent3 from './ShowComponent3'
 
-export default function Component1() {
-    const props = useSpring({opacity: 1,
-                            config: { delay: 9000, duration: 1000 },
-                            from: {opacity: 0}
-                                })
-    return (
-        <animated.div style={props}>
-            <div style={c2Style}>
-                <h1>Component 2</h1>
-                <p>Lorem Ipsum is simply dummy text of the printing and 
-                    typesetting industry. Lorem Ipsum has been the industry's 
-                    standard dummy text ever since the 1500s, when an unknown 
-                    printer took a galley of type and scrambled it to make 
-                    a type specimen book. It has survived not only five centuries, 
-                    but also the leap into electronic typesetting, remaining essentially 
-                    unchanged.
-                </p>
-                {/* error using props in functional component. Must change to class component */}
-                <button style={btn} onClick= {this.props.toggle}>Toggle Component 3</button>
-            </div>
-        </animated.div>
-    )
+
+
+export default class Component2 extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return <div>
+            <showComponent3 />
+        </div>
+    }
 }
 
-const c2Style = {
-    background: 'slateblue',
-    color: 'white',
-    padding: '1.5rem',
-    textAlign: 'center',
-}
 
-const btn = {
-    background: '#333',
-    color: '#fff',
-    padding: '1rem 2rem',
-    border: 'none',
-    textTransform: 'uppercase',
-    margin:'15px 0'
-
-}
---
+++ animation-exercise-spring/src/components/ShowComponent3.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { useSpring, animated, interpolate } from 'react-spring/renderprops';
+
+export default function showComponent3() {
+    const props = useSpring (
+        { opacity: 1,
+        config: { delay: 9000, duration: 1000 },
+        from: {opacity: 0}
+        })
+
+        return (
+            <animated.div style={props}>
+                <div style={c2Style}>
+                    <h1>Component 2</h1>
+                    <p>Lorem Ipsum is simply dummy text of the printing and 
+                        typesetting industry. Lorem Ipsum has been the 
+                        industry's standard dummy text ever since the 1500s, 
+                        when an unknown printer took a galley of type and 
+                        scrambled it to make a type specimen book. It has 
+                        survived not only five centuries, but also the leap 
+                        into electronic typesetting, remaining essentially 
+                        unchanged. It was popularised in the 1960s with the 
+                        release of Letraset sheets containing Lorem Ipsum passages, 
+                        and more recently with desktop publishing software like Aldus 
+                        PageMaker including versions of Lorem Ipsum.
+                    </p>
+                    {/* error using props in functional component. Must change to class component */}
+                    <button style={btn} onClick= {this.props.toggle}>Toggle Component 3</button>
+                </div>
+            </animated.div>
+        )
+
+}
+
+const c2Style = {
+    background: 'slateblue',
+    color: 'white',
+    padding: '1.5rem',
+    textAlign: 'center',
+}
+
+const btn = {
+    background: '#333',
+    color: '#fff',
+    padding: '1rem 2rem',
+    border: 'none',
+    textTransform: 'uppercase',
+    margin:'15px 0'
+
+}
